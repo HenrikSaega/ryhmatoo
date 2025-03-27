429cf1e18d9cf071bdd5063111bbe39e74757c8e
Henrik <Henriksaega@gmail.com>
2025-03-27 10:49:52 +0200

    Merge remote-tracking branch 'origin/dev-kaspar' into devHenrik

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,22 @@
 import { useNavigate } from "react-router-dom"
-import '../assets/styles/Home.css'
 
 const Home = () => {
   const navigate = useNavigate();
   return (
-    <div className="home">
-      <div className="title-container">
-        <h1 className="title col-md-12">GET-Recipe</h1>
-      </div>
-      <div className="row justify-content-center">
-        <div className="buttons-container col-12 col-md-8">
-          <button className="btn button mx-2 mt-5" onClick={() => navigate('/search')}>
-            Search Recipes
-          </button>
-          <button className="btn button mx-3 mt-5" onClick={() => navigate('/saved')}>
-            Saved Recipes
-          </button>
+    <div className="main">
+      <div className="home">
+        <div className="title-container">
+          <h1 className="title col-md-12">GET-Recipe</h1>
+        </div>
+        <div className="row justify-content-center">
+          <div className="buttons-container col-12 col-md-8">
+            <button className="btn button mx-2 mt-5" onClick={() => navigate('/search')}>
+              Search Recipes
+            </button>
+            <button className="btn button mx-3 mt-5" onClick={() => navigate('/saved')}>
+              Saved Recipes
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/SavedRecipes.js
+++ src/pages/SavedRecipes.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import '../assets/styles/main.css'
 
 const SavedRecipes = () => {
   const navigate = useNavigate();
